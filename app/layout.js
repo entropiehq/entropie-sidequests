bd59753126ff62dbc53ff0f2c6dd2c70dc026700
entropiehq <hello@entropies.ch>
2026-05-24 11:07:57 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Side Quest Generator",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
